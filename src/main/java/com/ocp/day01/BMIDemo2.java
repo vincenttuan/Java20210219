f34cc0a82edeb5cd8ee5f1d2ba1903606ba9601e
teacher <teacher@192.168.1.180>
2021-02-19 20:46:29 +0800

    Java OCP

--- src/main/java/com/ocp/day01/BMIDemo2.java
+++ src/main/java/com/ocp/day01/BMIDemo2.java
@@ -2,14 +2,13 @@ package com.ocp.day01;
 
 public class BMIDemo2 {
     public static void main(String[] args) {
-        
+        printBMI("John", 170, 60);
+        printBMI("Mary", 160, 48);
+        printBMI("Helen", 165, 65);
     }
     
-    public static void printBMI() {
+    public static void printBMI(String name, double height, double weight) {
         // 計算BMI
-        String name = "John";
-        double height = 170; // 170.0
-        double weight = 60; 
         double result = weight / Math.pow(height/100, 2);
         System.out.printf("%s 的身高: %.1f 體重: %.1f bmi: %.2f\n", 
                 name, height, weight, result);
