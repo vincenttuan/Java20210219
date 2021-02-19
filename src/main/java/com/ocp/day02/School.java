61bec84a9bb6109a68023999abbb09446793f096
teacher <teacher@192.168.1.180>
2021-02-19 21:40:51 +0800

    Java OCP

--- src/main/java/com/ocp/day02/School.java
+++ src/main/java/com/ocp/day02/School.java
@@ -6,10 +6,11 @@ public class School {
         Student s2 = new Student();
         s1.print();
         s2.print();
-        s1.schoolName = "台大";
+//        s1.schoolName = "師大";
+//        s2.schoolName = "成大";
+        Student.schoolName = "成大";
         s1.name = "John";
         s1.age = 20;
-        s2.schoolName = "台大";
         s2.name = "Mary";
         s2.age = 18;
         s1.print();
--- src/main/java/com/ocp/day02/Student.java
+++ src/main/java/com/ocp/day02/Student.java
@@ -1,8 +1,10 @@
 package com.ocp.day02;
 // Data class
+// 物件 = 實體, 實例, instance
 public class Student {
-    // 屬性
-    public String schoolName; // 學校
+    // 屬性/變數/資產/欄位
+    // static : 類別的, 靜態的
+    public static String schoolName; // 學校
     public String name; // 姓名
     public int age; // 年齡
     // 方法
