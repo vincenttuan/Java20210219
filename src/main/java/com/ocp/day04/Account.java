08bb48b371fdea4a90dbf9e4af54d52b62fe29b8
teacher <teacher@192.168.1.180>
2021-02-22 20:50:12 +0800

    Java OCP

--- src/main/java/com/ocp/day04/Account.java
+++ src/main/java/com/ocp/day04/Account.java
@@ -35,5 +35,28 @@ public class Account {
         money -= x;
     }
     
+    // 存款的方法
+    public void deposit(int x) {
+        System.out.printf("存款 $%d ", x);
+        // 驗證存款要件
+        // 1.驗證存款金額是否<=0
+        if(x <= 0) {
+            System.out.println("存款金額錯誤");
+            return; // 中斷方法
+        }
+        // 2.驗證存款金額是否是100的倍數
+        if(x % 100 != 0) {
+            System.out.println("存款金額必須是100的倍數");
+            return; // 中斷方法
+        }
+        // 3.驗證x是否大於10萬?
+        if(x > 10_0000) {
+            System.out.println("存款金額不可超過10萬");
+            return;
+        }
+        // 進行存款程序
+        System.out.println("成功");
+        money += x;
+    }
 
 }
