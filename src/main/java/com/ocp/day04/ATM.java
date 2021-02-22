38b5d6a00458633ec5ba2f47cc62fda6edce90b0
teacher <teacher@192.168.1.180>
2021-02-22 19:50:30 +0800

    Java OCP

--- src/main/java/com/ocp/day04/ATM.java
+++ src/main/java/com/ocp/day04/ATM.java
@@ -2,6 +2,7 @@ package com.ocp.day04;
 
 public class ATM {
     public static void main(String[] args) {
-        
+        Account act = new Account();
+        act.printMoney();
     }
 }
--- src/main/java/com/ocp/day04/Account.java
+++ src/main/java/com/ocp/day04/Account.java
@@ -2,5 +2,12 @@ package com.ocp.day04;
 
 // 銀行帳戶
 public class Account {
-    int money = 100; // 預設有100元
+    // 預設有100元
+    private int money = 100; 
+    
+    // 餘額查詢的方法
+    public void printMoney() {
+        System.out.printf("$%d\n", money);
+    }
+
 }
