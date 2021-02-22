4e46e3d7b94492d10c221bbf4683005dd1c44301
teacher <teacher@192.168.1.180>
2021-02-22 20:53:18 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day04/ATMSystem.java
@@ -0,0 +1,15 @@
+package com.ocp.day04;
+
+public class ATMSystem {
+    public static void main(String[] args) {
+        
+    }
+    public static void menu() {
+        System.out.println("-----------------");
+        System.out.println("1. 存款");
+        System.out.println("2. 提款");
+        System.out.println("3. 查詢");
+        System.out.println("0. 離開");
+        System.out.println("-----------------");
+    }
+}
