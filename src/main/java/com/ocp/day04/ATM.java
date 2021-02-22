db3b18623df8abf5b8316f7d215219bb32414e0a
teacher <teacher@192.168.1.180>
2021-02-22 19:56:58 +0800

    Java OCP

--- src/main/java/com/ocp/day04/ATM.java
+++ src/main/java/com/ocp/day04/ATM.java
@@ -4,5 +4,9 @@ public class ATM {
     public static void main(String[] args) {
         Account act = new Account();
         act.printMoney();
+        act.withdrawal(70);
+        act.printMoney();
+        act.withdrawal(50);
+        act.printMoney();
     }
 }
--- src/main/java/com/ocp/day04/Account.java
+++ src/main/java/com/ocp/day04/Account.java
@@ -7,7 +7,19 @@ public class Account {
     
     // 餘額查詢的方法
     public void printMoney() {
-        System.out.printf("$%d\n", money);
+        System.out.printf("餘額 $%d\n", money);
     }
+    
+    // 提款的方法
+    public void withdrawal(int x) {
+        System.out.printf("提款 $ %d ", x);
+        if(x <= money) {
+            System.out.println("成功");
+            money -= x;
+        } else {
+            System.out.println("失敗 (餘額不足)");
+        }
+    }
+    
 
 }
