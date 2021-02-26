a239056dda5255f5a2b42f5bbbaf7eeb68ca75fd
teacher <teacher@192.168.1.180>
2021-02-26 21:19:57 +0800

    Java OCP

--- src/main/java/com/ocp/day05/ArrayDemo4.java
+++ src/main/java/com/ocp/day05/ArrayDemo4.java
@@ -8,6 +8,20 @@ public class ArrayDemo4 {
         int[] scores1 = {81, 90, 90, 100, 40, 50};
         System.out.printf("數學: %s\n", Arrays.toString(scores1));
         // 找出最高分與最低分
-        
+        int max = 0;
+        // Java 7
+        for(int x : scores1) {
+            if(x > max) {
+                max = x;
+            }
+        }
+        System.out.printf("最高分: %d\n", max);
+        int min = max;
+        for(int x : scores1) {
+            if(x < min) {
+                min = x;
+            }
+        }
+        System.out.printf("最低分: %d\n", min);
     }
 }
