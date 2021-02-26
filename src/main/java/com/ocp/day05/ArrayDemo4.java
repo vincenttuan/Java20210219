8c1cdf215419280a042060f13f82e0a4b409e12e
teacher <teacher@192.168.1.180>
2021-02-26 21:08:08 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day05/ArrayDemo4.java
@@ -0,0 +1,13 @@
+package com.ocp.day05;
+
+import java.util.Arrays;
+
+public class ArrayDemo4 {
+    public static void main(String[] args) {
+        // 全班有6個人此次考數學的成績列表
+        int[] scores1 = {81, 90, 90, 100, 40, 50};
+        System.out.printf("數學: %s\n", Arrays.toString(scores1));
+        // 找出最高分與最低分
+        
+    }
+}
