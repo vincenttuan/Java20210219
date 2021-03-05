5666d7d51a4f3e42ccce0f81a4497e8b4161f90a
teacher <teacher@192.168.1.180>
2021-03-05 19:11:06 +0800

    Java OCP

--- src/main/java/com/ocp/day06/StringCompare.java
+++ src/main/java/com/ocp/day06/StringCompare.java
@@ -6,6 +6,7 @@ public class StringCompare {
         String s2 = new String("Java");
         String s3 = "Java";
         String s4 = "Java";
+        String s5 = "JaVA";
         System.out.println(s1 == s2);
         System.out.println(s1.equals(s2));
         System.out.println(s3 == s4);
@@ -13,5 +14,8 @@ public class StringCompare {
         System.out.println(s1 == s3);
         System.out.println(s1.equals(s3));
         System.out.println(s1.intern() == s3);
+        System.out.println(s2.equalsIgnoreCase(s5)); // 忽略大小寫的比較
+        System.out.println(s4.toUpperCase() == s5.toUpperCase()); // 全部轉大寫再比較
+        System.out.println(s4.toLowerCase().equals(s5.toLowerCase())); // 全部轉小寫再比較
     }
 }
