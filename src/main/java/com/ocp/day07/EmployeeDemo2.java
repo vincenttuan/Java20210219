5d951370d204ba8b56fec18b24e07558f9b69da1
teacher <teacher@192.168.1.180>
2021-03-08 21:32:11 +0800

    Java OCP

--- src/main/java/com/ocp/day07/EmployeeDemo2.java
+++ src/main/java/com/ocp/day07/EmployeeDemo2.java
@@ -1,8 +1,19 @@
 package com.ocp.day07;
 
 public class EmployeeDemo2 {
+
     public static void main(String[] args) {
         Employee e1 = new Employee("John", "Java", 30, 75000);
-        System.out.println(e1); // e1.toString()
+        Employee e2 = new Employee("Mary", "Python", 28, 55000);
+        Employee e3 = new Employee("Helen", "Java", 35, 95000);
+        Employee e4 = new Employee("Anita", "Python", 32, 105000);
+        Employee e5 = new Employee("Vincent", "Java", 28, 85000);
+        //System.out.println(e1); // e1.toString()
+        Employee[] employees = {e1, e2, e3, e4, e5};
+        System.out.println(employees);
+        // for-each
+        for(Employee e : employees) {
+            System.out.println(e);
+        }
     }
 }
