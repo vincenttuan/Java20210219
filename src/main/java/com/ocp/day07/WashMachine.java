fc209975e8ce002cbcc1ea3f26a6cdacb826f6c5
teacher <teacher@192.168.1.180>
2021-03-08 19:44:59 +0800

    Java OCP

--- src/main/java/com/ocp/day07/WashMachine.java
+++ src/main/java/com/ocp/day07/WashMachine.java
@@ -3,15 +3,22 @@ package com.ocp.day07;
 public class WashMachine {
     // 開門
     public void open() {
-        System.out.println("開門-放衣服");
+        System.out.println("開門");
+    }
+    // 放衣服
+    public void putClothes() {
+        System.out.println("放衣服");
     }
     // 關門
     public void close() {
         System.out.println("關門");
     }
-    // 洗衣程序
-    public void play() {
+    // 放水
+    public void water() {
         System.out.println("注水");
+    }
+    // 洗衣
+    public void play() {
         System.out.println("開始洗衣服");
         System.out.println("衣服洗完了");
     }
--- src/main/java/com/ocp/day07/WashMachinePlay.java
+++ src/main/java/com/ocp/day07/WashMachinePlay.java
@@ -1,5 +1,13 @@
 package com.ocp.day07;
 
 public class WashMachinePlay {
+    public static void main(String[] args) {
+        WashMachine w1 = new WashMachine();
+        w1.open();
+        w1.putClothes();
+        w1.close();
+        w1.water();
+        w1.play();
+    }
     
 }
