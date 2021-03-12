2e1c4d4809ada32a135a6c105107b4cd539f67aa
teacher <teacher@192.168.1.180>
2021-03-12 21:22:33 +0800

    Java OCP

--- src/main/java/com/ocp/day09/Main.java
+++ src/main/java/com/ocp/day09/Main.java
@@ -8,7 +8,7 @@ public class Main {
         
         Employee mary = new Employee("Mary", new Salary(70000, 8000));
         
-        System.out.printf("%s 的薪資是 $%d\n", john.getName(), john.getSalary().getMoney());
-        System.out.printf("%s 的薪資是 $%d\n", mary.getName(), mary.getSalary().getMoney());
+        System.out.printf("%s 的薪資是 $%,d\n", john.getName(), john.getSalary().getMoney());
+        System.out.printf("%s 的薪資是 $%,d\n", mary.getName(), mary.getSalary().getMoney());
     }
 }
