ff4d6dec575efe8cede62d66efb8d18b253ac8fb
teacher <teacher@192.168.1.180>
2021-03-12 19:17:40 +0800

    Java OCP

--- src/main/java/com/ocp/day08/BigData.java
+++ src/main/java/com/ocp/day08/BigData.java
@@ -5,18 +5,21 @@ public class BigData {
     private String author = "交易所";
     private String version = "1.0";
     
-    // 物件初始區段
+    // 物件初始區段 (會自動將此區段程式碼配置到所有的建構子內)
     {
-        if(new Date().getTime() % 2 == 0) {
+        if(new Date().getTime() % 3 == 0) {
             data = "盤中資料";
         } else {
             data = "盤後資料";
         }
+    }
+    
+    BigData() {
+        //data = "盤前資料";
     }
 
     @Override
     public String toString() {
         return "BigData{" + "data=" + data + ", author=" + author + ", version=" + version + '}';
     }
-    
 }
