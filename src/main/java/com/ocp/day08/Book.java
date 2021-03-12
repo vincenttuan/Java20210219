573f329ea6ce5b0bb5a4b6d1b48bc65e905f4ccb
teacher <teacher@192.168.1.180>
2021-03-12 19:57:09 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day08/Book.java
@@ -0,0 +1,12 @@
+package com.ocp.day08;
+
+public class Book {
+    // 物件成員
+    public int getPages() {
+        return 100;
+    }
+    // 類別成員
+    public static String getPublich() {
+        return "碁峯資訊";
+    }
+}
