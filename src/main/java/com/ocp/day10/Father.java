f8c2c11fa0859b192855b76dd84c4c43a585c278
teacher <teacher@192.168.1.180>
2021-03-15 19:31:16 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day10/Father.java
@@ -0,0 +1,10 @@
+package com.ocp.day10;
+
+public class Father {
+    public void hobby() {
+        System.out.println("Golf");
+    }
+    private void asset() {
+        System.out.println("父親的私人產業");
+    }
+}
