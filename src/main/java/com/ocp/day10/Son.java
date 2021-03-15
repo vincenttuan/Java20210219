c4992040db88bddfc0b3421a9fc4094de82c4267
teacher <teacher@192.168.1.180>
2021-03-15 19:37:21 +0800

    Java OCP

--- src/main/java/com/ocp/day10/Son.java
+++ src/main/java/com/ocp/day10/Son.java
@@ -1,5 +1,11 @@
 package com.ocp.day10;
 
 public class Son extends Father {
+
+    @Override
+    public void hobby() {
+        //super.hobby();
+        System.out.println("LoL");
+    }
     
 }
