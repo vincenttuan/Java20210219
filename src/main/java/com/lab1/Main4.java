6a864345fd974ff5067c4f175bcbc29821d5ed9f
teacher <teacher@192.168.1.180>
2021-03-19 21:33:35 +0800

    Java OCP

---
+++ src/main/java/com/lab1/Main4.java
@@ -0,0 +1,14 @@
+package com.lab1;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Employee employee = new Employee(38000);
+        Manager  manager  = new Manager(68000);
+        Director director = new Director(88000);
+        
+        EmployeeUtil util = new EmployeeUtil();
+        System.out.println(util.getAllSalary(employee, manager, director));
+        System.out.println(util.getAllBudget(manager, director));
+        System.out.println(util.getAllStockOptions(director));
+    }
+}
