fd2a73b58edbce2a7808d30de5f6ddd1a29a9785
teacher <teacher@192.168.1.180>
2021-03-19 20:50:27 +0800

    Java OCP

--- src/main/java/com/lab1/EmployeeUtil.java
+++ src/main/java/com/lab1/EmployeeUtil.java
@@ -7,14 +7,17 @@ import java.util.stream.Stream;
 public class EmployeeUtil {
     // 取得所有員工的薪資
     public int getAllSalary(Employee... employees) {
-        // Java 7
-//        int sum = 0;
-//        for(Employee e : employees) {
-//            sum += e.getSalary();
-//        }
-//        return sum;
         // Java 8
         //return Stream.of(employees).mapToInt(e -> e.getSalary()).sum();
         return Stream.of(employees).mapToInt(Employee::getSalary).sum();
     }
+    // 取得所有經理(Manager)的薪資
+    public int getAllSalary(Manager... managers) {
+        return Stream.of(managers).mapToInt(Manager::getSalary).sum();
+    }
+    // 取得所有董事(Director)的薪資
+    public int getAllSalary(Director... directors) {
+        return Stream.of(directors).mapToInt(Director::getSalary).sum();
+    }
+    
 }
