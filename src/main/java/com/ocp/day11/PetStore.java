823a4e9eb1bd132d1b2b64b64e34c30b3b1fed97
teacher <teacher@192.168.1.180>
2021-03-22 19:23:47 +0800

    Java OCP

--- src/main/java/com/ocp/day11/PetStore.java
+++ src/main/java/com/ocp/day11/PetStore.java
@@ -1,7 +1,16 @@
 package com.ocp.day11;
 
+import java.util.stream.Stream;
+
 public class PetStore {
     public static void main(String[] args) {
         System.out.println(Animal.name);
+        Dog dog = new Dog();
+        Pig pig = new Pig();
+        Bird bird = new Bird();
+        playSound(dog, pig, bird);
+    }
+    public static void playSound(Animal... animals) {
+        Stream.of(animals).forEach(a -> a.sound());
     }
 }
