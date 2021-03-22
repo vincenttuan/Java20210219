1c55feff330124d8b241fced15c158eec1bc0217
teacher <teacher@192.168.1.180>
2021-03-22 21:02:41 +0800

    Java OCP

---
+++ src/main/java/com/lab/decorator/Bread.java
@@ -0,0 +1,18 @@
+package com.lab.decorator;
+
+public class Bread extends Food {
+    public Bread() {
+        name = "麵包";
+        price = 40;
+    }
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getPrice() {
+        return price;
+    }
+    
+}
