1643289e6b0a40074422ac08b60961dfcd9aecb8
teacher <teacher@192.168.1.180>
2021-03-22 20:51:55 +0800

    Java OCP

--- src/main/java/com/lab/lab1/Employee.java
+++ src/main/java/com/lab/lab1/Employee.java
@@ -1,4 +1,4 @@
-package com.lab1;
+package com.lab.lab1;
 
 
 public class Employee {
--- src/main/java/com/lab/lab1/Main3.java
+++ src/main/java/com/lab/lab1/Main3.java
@@ -1,4 +1,4 @@
-package com.lab1;
+package com.lab.lab1;
 
 public class Main3 {
     public static void main(String[] args) {
