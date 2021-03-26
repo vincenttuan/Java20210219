2fc2266fc79e3e91919481baab4a3c96e45f7b54
teacher <teacher@192.168.1.180>
2021-03-26 18:51:47 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day13/Bird.java
@@ -0,0 +1,10 @@
+package com.ocp.day13;
+
+public class Bird implements Animal {
+
+    @Override
+    public void move() {
+        System.out.println("飛");
+    }
+    
+}
---
+++ src/main/java/com/ocp/day13/Dog.java
@@ -0,0 +1,10 @@
+package com.ocp.day13;
+
+public class Dog implements Animal {
+
+    @Override
+    public void move() {
+        System.out.println("跑");
+    }
+    
+}
