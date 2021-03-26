a1285da5bf4d8783e344e0b4d97480c7a3aaa87a
teacher <teacher@192.168.1.180>
2021-03-26 19:04:21 +0800

    Java OCP

--- src/main/java/com/ocp/day13/Animal.java
+++ src/main/java/com/ocp/day13/Animal.java
@@ -2,4 +2,5 @@ package com.ocp.day13;
 
 public interface Animal {
     void move();
+    void eat();
 }
---
+++ src/main/java/com/ocp/day13/EDog.java
@@ -0,0 +1,16 @@
+package com.ocp.day13;
+
+public class EDog implements Animal {
+
+    @Override
+    public void move() {
+        System.out.println("隨你所欲");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("我吃電");
+    }
+    
+    
+}
