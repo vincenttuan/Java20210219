43bf3e05957057e2c094f2c24ba9df711fbf4f7e
teacher <teacher@192.168.1.180>
2021-03-26 21:30:42 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day14/Anonymous5.java
@@ -0,0 +1,13 @@
+package com.ocp.day14;
+
+interface BMI {
+    double calc(double h, double w);
+}
+
+public class Anonymous5 {
+    public static void main(String[] args) {
+        // 請用 Lambda 語法實現 BMI
+        // 身高 170, 體重 60
+        
+    }
+}
