0c4bdd84fa78b942037038b8ab54fbbca2620e0c
teacher <teacher@192.168.1.180>
2021-03-26 20:55:58 +0800

    Java OCP

--- src/main/java/com/ocp/day14/Anonymous.java
+++ src/main/java/com/ocp/day14/Anonymous.java
@@ -12,12 +12,13 @@ public class Anonymous {
                 return 5_0000;
             }
         };
-        
         Salary salary2 = new Salary() {
             @Override
             public int amount() {
                 return 8_0000;
             }
         };
+        System.out.println(salary.amount());
+        System.out.println(salary2.amount());
     }
 }
