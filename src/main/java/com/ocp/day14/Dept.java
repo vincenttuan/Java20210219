a2dd54debca2350e2e4051c4c515a1d32a9b3e78
teacher <teacher@192.168.1.180>
2021-03-26 19:20:14 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day14/Dept.java
@@ -0,0 +1,11 @@
+package com.ocp.day14;
+
+public class Dept { // 部門
+    String name = "軟體部門";
+    class Job { // 工作
+        void work() {
+            String name = "寫程式";
+            System.out.println(name);
+        }
+    }
+}
