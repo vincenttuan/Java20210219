57f96f5cbadac95fb10f6ebb99c0f13e37e846e4
teacher <teacher@192.168.1.180>
2021-03-29 19:35:15 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day15/ConsumerDemo.java
@@ -0,0 +1,13 @@
+package com.ocp.day15;
+
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        int[] radius = {10, -5, 15};
+                
+    }
+    public static void print(int[] radius, Consumer consumer) {
+        
+    }
+}
