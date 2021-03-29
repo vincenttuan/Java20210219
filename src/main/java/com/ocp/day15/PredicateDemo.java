fde5966e60064c930c33aac8d729bb043d2e05b6
teacher <teacher@192.168.1.180>
2021-03-29 20:53:38 +0800

    Java OCP

--- src/main/java/com/ocp/day15/PredicateDemo.java
+++ src/main/java/com/ocp/day15/PredicateDemo.java
@@ -2,20 +2,25 @@ package com.ocp.day15;
 
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class PredicateDemo {
     public static void main(String[] args) {
-        Integer[] radius = {5, 10, 15};
+        Integer[] radius = {5, -10, 15};
         Consumer<Double> consumer = System.out::println;
         Function<Integer, Double> function = (t) -> Math.pow(t, 2) * Math.PI;
-        print(radius, consumer, function);
+        Predicate<Integer> predicate = (t) -> t >= 0;
+        print(radius, consumer, function, predicate);
     }
     public static void print(Integer[] radius, 
                              Consumer<Double> consumer,
-                             Function<Integer, Double> function) {
+                             Function<Integer, Double> function,
+                             Predicate<Integer> predicate) {
         for(Integer r : radius) {
-            Double area = function.apply(r);
-            consumer.accept(area);
+            if(predicate.test(r)) {
+                Double area = function.apply(r);
+                consumer.accept(area);
+            }
         }
     }
 }
