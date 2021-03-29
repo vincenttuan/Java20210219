5239207124bbce98805936ff5c691e068fd422fc
teacher <teacher@192.168.1.180>
2021-03-29 21:13:47 +0800

    Java OCP

--- src/main/java/com/ocp/day15/LambdaDemo.java
+++ src/main/java/com/ocp/day15/LambdaDemo.java
@@ -1,13 +1,15 @@
 package com.ocp.day15;
 
 import java.util.stream.Stream;
+import java.util.function.Supplier;
 
 public class LambdaDemo {
     public static void main(String[] args) {
         Integer[] radius = {5, -10, 15};
+        Supplier<Double> pi = () -> Math.PI; // Supplier
         Stream.of(radius)
                 .filter(t -> t > 0) // Predicate
-                .mapToDouble(t -> Math.pow(t, 2) * Math.PI) // Function
+                .mapToDouble(t -> Math.pow(t, 2) * pi.get()) // Function
                 .forEach(System.out::println); // Consumer
     }
 }
