04180b6aae75a6d51c9ab9dddf54517afe8865fe
teacher <teacher@192.168.1.180>
2021-03-29 19:40:18 +0800

    Java OCP

--- src/main/java/com/ocp/day15/ConsumerDemo.java
+++ src/main/java/com/ocp/day15/ConsumerDemo.java
@@ -4,10 +4,13 @@ import java.util.function.Consumer;
 
 public class ConsumerDemo {
     public static void main(String[] args) {
-        int[] radius = {10, -5, 15};
-                
+        Integer[] radius = {10, -5, 15};
+        Consumer<Integer> consumer = (t) -> System.out.println(t);
+        print(radius, consumer);
     }
-    public static void print(int[] radius, Consumer consumer) {
-        
+    public static void print(Integer[] radius, Consumer<Integer> consumer) {
+        for(Integer r : radius) {
+            consumer.accept(r);
+        }
     }
 }
