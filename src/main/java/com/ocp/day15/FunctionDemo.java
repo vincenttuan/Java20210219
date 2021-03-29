f86a405f4f456f0d43948c14016cf379a35dbd6a
teacher <teacher@192.168.1.180>
2021-03-29 20:35:17 +0800

    Java OCP

--- src/main/java/com/ocp/day15/FunctionDemo.java
+++ src/main/java/com/ocp/day15/FunctionDemo.java
@@ -9,6 +9,8 @@ public class FunctionDemo {
         Consumer<Double> consumer = System.out::println;
         Function<Integer, Double> function = (t) -> Math.pow(t, 2) * Math.PI;
         print(radius, consumer, function);
+        
+        print(radius, (t)->System.out.printf("%.2f\n", t), function);
     }
     public static void print(Integer[] radius, 
                              Consumer<Double> consumer,
