28f4a5ef1bd7588c9ebfc3cfc5ccc7a9e1aa8a66
teacher <teacher@192.168.1.180>
2021-04-09 18:50:29 +0800

    Java OCP

--- src/main/java/com/ocp/day16/InnerClass.java
+++ src/main/java/com/ocp/day16/InnerClass.java
@@ -1,16 +1,22 @@
 package com.ocp.day16;
 
 public class InnerClass {
+    int x = 100;
+    static int sx = 200;
     // 一般內部類別
     class MyInner {
         void print() {
             System.out.println("MyInner");
+            System.out.println(InnerClass.this.x);
+            System.out.println(InnerClass.sx);
+            MyStaticInner.print();
         }
     }
     // 靜態內部類別
     static class MyStaticInner {
         static void print() {
             System.out.println("MyStaticInner");
+            System.out.println(InnerClass.sx);
         }
     }
 }
