4e27cb571cafb68ead28126f7d8386c23907b7de
teacher <teacher@192.168.1.180>
2021-04-09 19:04:03 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day16/Person.java
@@ -0,0 +1,41 @@
+package com.ocp.day16;
+
+public class Person {
+    private String name;
+    private Double h;
+    private Double w;
+
+    public Person() {
+    }
+    
+    public Person(String name, Double h, Double w) {
+        this.name = name;
+        this.h = h;
+        this.w = w;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getH() {
+        return h;
+    }
+
+    public void setH(Double h) {
+        this.h = h;
+    }
+
+    public Double getW() {
+        return w;
+    }
+
+    public void setW(Double w) {
+        this.w = w;
+    }
+    
+}
