4334889111ee984175aa647b0bc7fd2938754450
teacher <teacher@192.168.1.180>
2021-04-09 21:00:19 +0800

    Java OCP

--- src/main/java/com/ocp/day16/Sex.java
+++ src/main/java/com/ocp/day16/Sex.java
@@ -1,7 +1,6 @@
 package com.ocp.day16;
 
-public interface Sex {
-    int man = 1;
-    int female = 2;
+public enum Sex {
+    man, female
 }
 
--- src/main/java/com/ocp/day16/SexMain.java
+++ src/main/java/com/ocp/day16/SexMain.java
@@ -2,15 +2,15 @@ package com.ocp.day16;
 
 public class SexMain {
     public static void main(String[] args) {
-        //checkSex(1);
         checkSex(Sex.man);
+        checkSex(Sex.female);
     }
-    public static void checkSex(int value) {
-        switch(value) {
-            case Sex.man:
+    public static void checkSex(Sex sex) {
+        switch(sex) {
+            case man:
                 System.out.println("我是男生");
                 break;
-            case Sex.female:
+            case female:
                 System.out.println("我是女生");
                 break;
         }
