01e491628dd7bfab8119c10cf46b624306dd4b57
teacher <teacher@192.168.1.180>
2021-04-09 18:55:08 +0800

    Java OCP

--- src/main/java/com/ocp/day16/InnerClass.java
+++ src/main/java/com/ocp/day16/InnerClass.java
@@ -19,4 +19,11 @@ public class InnerClass {
             System.out.println(InnerClass.sx);
         }
     }
+    // 主程式
+    public static void main(String[] args) {
+        InnerClass.MyInner mi = new InnerClass().new MyInner();
+        mi.print();
+        InnerClass.MyStaticInner msi = new InnerClass.MyStaticInner();
+        msi.print();
+    }
 }
