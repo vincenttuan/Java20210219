63edc5c9d8facf0d3ab5243ba50191a992d7c29e
teacher <teacher@192.168.1.180>
2021-04-09 19:47:41 +0800

    Java OCP

--- src/main/java/com/ocp/day16/Person.java
+++ src/main/java/com/ocp/day16/Person.java
@@ -1,5 +1,7 @@
 package com.ocp.day16;
 
+import java.util.Objects;
+
 public class Person {
     private String name;
     private Double h;
@@ -15,22 +17,39 @@ public class Person {
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if(obj instanceof Person) {
-            Person p = (Person)obj;
-            if(name.equals(p.name) && 
-               h.doubleValue() == p.h.doubleValue() && 
-               w.doubleValue() == p.w.doubleValue()) {
-                return true;
-            }
-        }
-        return false; 
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + Objects.hashCode(this.name);
+        hash = 83 * hash + Objects.hashCode(this.h);
+        hash = 83 * hash + Objects.hashCode(this.w);
+        return hash;
     }
 
     @Override
-    public int hashCode() {
-        return 7 * 11 * name.hashCode() * h.intValue() * w.intValue();
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Person other = (Person) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.h, other.h)) {
+            return false;
+        }
+        if (!Objects.equals(this.w, other.w)) {
+            return false;
+        }
+        return true;
     }
+
+    
 
     public String getName() {
         return name;
