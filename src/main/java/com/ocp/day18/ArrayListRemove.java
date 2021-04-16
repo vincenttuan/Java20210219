c1a146fff028d37fcfd095bd85f88ce167e0d00b
teacher <teacher@192.168.1.180>
2021-04-16 20:00:08 +0800

    Java OCP

--- src/main/java/com/ocp/day18/ArrayListRemove.java
+++ src/main/java/com/ocp/day18/ArrayListRemove.java
@@ -10,7 +10,7 @@ public class ArrayListRemove {
         lotto.add(1); // 1
         lotto.add(4); // 2
         lotto.add(3); // 3
-        lotto.add(4); // 4
+        lotto.add(5); // 4
         System.out.println(lotto);
         lotto.remove(1);
         System.out.println(lotto);
