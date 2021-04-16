b7a2f1a2c148031a6bbf9a4f9adfbe4bd7e718f6
teacher <teacher@192.168.1.180>
2021-04-16 19:24:35 +0800

    Java OCP

--- src/main/java/com/ocp/day18/Lotto539.java
+++ src/main/java/com/ocp/day18/Lotto539.java
@@ -1,8 +1,18 @@
 package com.ocp.day18;
 
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.Set;
+
 public class Lotto539 {
     // 樂透 539 電腦選號 (1~39取出5個不重複的數字)
     public static void main(String[] args) {
-        
+        Random r = new Random();
+        Set<Integer> lotto = new LinkedHashSet<>();
+        while (lotto.size() < 5) {            
+            int n = r.nextInt(39) + 1;
+            lotto.add(n);
+        }
+        System.out.println(lotto);
     }
 }
