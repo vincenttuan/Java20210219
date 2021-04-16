9a1a7a04ff41f138b8434881fca8262e7f986a36
teacher <teacher@192.168.1.180>
2021-04-16 21:34:28 +0800

    Java OCP

--- src/main/java/com/ocp/day18/LottoService.java
+++ src/main/java/com/ocp/day18/LottoService.java
@@ -1,15 +1,24 @@
 package com.ocp.day18;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
 public class LottoService {
+    static Random r = new Random();
     public static Set<Integer> get539() {
         Set<Integer> lotto = new LinkedHashSet<>();
-        Random r = new Random();
         while(lotto.size() < 5) {
-            lotto.add(r.nextInt(39)+1);
+            lotto.add(r.nextInt(39)+1); // 1~39
+        }
+        return lotto;
+    }
+    public static List<Integer> get4Stars() {
+        List<Integer> lotto = new ArrayList<>();
+        for(int i=0;i<4;i++) {
+            lotto.add(r.nextInt(10)); // 0~9
         }
         return lotto;
     }
