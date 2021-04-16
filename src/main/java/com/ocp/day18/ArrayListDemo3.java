cd97c6cb38e8b4d9fb9d5f69ba57f3fc3b0a7c3c
teacher <teacher@192.168.1.180>
2021-04-16 20:49:57 +0800

    Java OCP

--- src/main/java/com/ocp/day18/ArrayListDemo3.java
+++ src/main/java/com/ocp/day18/ArrayListDemo3.java
@@ -1,8 +1,10 @@
 package com.ocp.day18;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.ListIterator;
+import java.util.Random;
+import java.util.Set;
 
 public class ArrayListDemo3 {
     // 1~5 取 3 個, 取後不放回
@@ -13,7 +15,17 @@ public class ArrayListDemo3 {
         lotto.add(3);
         lotto.add(4);
         lotto.add(5);
+        // 存放所選取的號碼
+        Set<Integer> ans = new LinkedHashSet<>();
         System.out.println(lotto);
-        
+        Random r = new Random();
+        for(int i=0;i<3;i++) {
+            int idx = r.nextInt(lotto.size());
+            int value = lotto.get(idx);
+            lotto.remove(idx);
+            ans.add(value);
+        }
+        System.out.println(lotto);
+        System.out.println(ans);
     }
 }
