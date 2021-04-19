429061604a454a99a8796b63761175366bd4944d
teacher <teacher@192.168.1.180>
2021-04-19 21:29:15 +0800

    Java OCP

--- src/main/java/com/ocp/day19/LinkedHashMapDemo2.java
+++ src/main/java/com/ocp/day19/LinkedHashMapDemo2.java
@@ -10,6 +10,8 @@ public class LinkedHashMapDemo2 {
         exams.put("數學", 80);
         exams.put("英文", 90);
         // 求總分
-        
+        exams.entrySet().stream().forEach(System.out::println);
+        int sum = exams.entrySet().stream().mapToInt(entry -> entry.getValue()).sum();
+        System.out.println(sum);
     }
 }
