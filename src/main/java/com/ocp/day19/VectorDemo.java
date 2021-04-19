aff88449b1ee85afb32a1dc97d065dd826ef51ba
teacher <teacher@192.168.1.180>
2021-04-19 19:48:01 +0800

    Java OCP

--- src/main/java/com/ocp/day19/VectorDemo.java
+++ src/main/java/com/ocp/day19/VectorDemo.java
@@ -4,10 +4,11 @@ import java.util.Vector;
 
 public class VectorDemo {
     public static void main(String[] args) {
-        Vector<Integer> v = new Vector();
-        for(int i=1;i<=10;i++) {
+        Vector<Integer> v = new Vector(641, 10);
+        for(int i=1;i<=641;i++) {
             v.add(i);
         }
+        v.add(100);
         System.out.println("size: " + v.size());
         System.out.println("capacity: " + v.capacity());
     }
