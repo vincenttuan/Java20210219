180fb21cd1e0eb435bea20acde1087fcd5e965fc
teacher <teacher@192.168.1.180>
2021-04-19 21:15:22 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day19/LinkedHashMapDemo2.java
@@ -0,0 +1,15 @@
+package com.ocp.day19;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LinkedHashMapDemo2 {
+    public static void main(String[] args) {
+        Map<String, Integer> exams = new LinkedHashMap<>();
+        exams.put("國文", 100);
+        exams.put("數學", 90);
+        exams.put("英文", 90);
+        // 求總分
+        
+    }
+}
