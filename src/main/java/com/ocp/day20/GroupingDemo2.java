08de5d2fbeb08dc34169c2deb399736c249d2462
teacher <teacher@192.168.1.180>
2021-04-23 21:20:42 +0800

    Java OCP

--- src/main/java/com/ocp/day20/GroupingDemo2.java
+++ src/main/java/com/ocp/day20/GroupingDemo2.java
@@ -2,14 +2,21 @@ package com.ocp.day20;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class GroupingDemo2 {
     public static void main(String[] args) {
         List<String> sex = Arrays.asList("男", "男", "男", "女", "女", "女", "男", "男");
         // 請問男女各有多少 ?
-        
-        List<Integer> score = Arrays.asList(100, 30, 80, 40, 90);
+        Map<String, Long> groupingBySex = sex.stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        System.out.println(groupingBySex);
+        List<Integer> score = Arrays.asList(100, 30, 100, 80, 40, 90);
         // 請問及格與不及格各有多少 ?
-        
+        Map<String, Long> groupingByScore = score.stream()
+                .collect(Collectors.groupingBy(e -> e >= 60?"及格":"不及格", Collectors.counting()));
+        System.out.println(groupingByScore);
     }
 }
