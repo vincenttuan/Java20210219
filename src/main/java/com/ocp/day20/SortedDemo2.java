027cd02f366e69b92b0bf60e8599ef30fb192e7f
teacher <teacher@192.168.1.180>
2021-04-23 19:37:30 +0800

    Java OCP

--- src/main/java/com/ocp/day20/SortedDemo2.java
+++ src/main/java/com/ocp/day20/SortedDemo2.java
@@ -11,9 +11,11 @@ public class SortedDemo2 {
         // 將及格的分數排序好的資料放入新集合中
         List<Integer> sortedAndPassList = list.stream()
                 .filter(e -> e >= 60)
-                .sorted() // 自然排序
+                //.sorted() // 自然排序
                 //.sorted(Comparator.naturalOrder()) // 自然排序
                 //.sorted(Comparator.reverseOrder()) // 反向排序
+                .sorted((o1, o2) -> o1.compareTo(o2)) // 自定義排序 1
+                //.sorted((o1, o2) -> o1 - o2) // 自定義排序 2
                 .collect(toList());
         System.out.println(sortedAndPassList);
         
