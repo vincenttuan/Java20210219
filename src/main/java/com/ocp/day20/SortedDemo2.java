c4364193dcc7024afe04424452a0cbbba861749e
teacher <teacher@192.168.1.180>
2021-04-23 19:32:47 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day20/SortedDemo2.java
@@ -0,0 +1,22 @@
+package com.ocp.day20;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import static java.util.stream.Collectors.toList;
+
+public class SortedDemo2 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(100, 40, 80, 30, 90);
+        // 將及格的分數排序好的資料放入新集合中
+        List<Integer> sortedAndPassList = list.stream()
+                .filter(e -> e >= 60)
+                .sorted() // 自然排序
+                //.sorted(Comparator.naturalOrder()) // 自然排序
+                //.sorted(Comparator.reverseOrder()) // 反向排序
+                .collect(toList());
+        System.out.println(sortedAndPassList);
+        
+        System.out.println(list);
+    }
+}
