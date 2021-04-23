98161e0fd799a7eb0a186d2a4e042d9a3d05bd33
teacher <teacher@192.168.1.180>
2021-04-23 20:56:13 +0800

    Java OCP

---
+++ src/main/java/com/ocp/day20/GroupingDemo2.java
@@ -0,0 +1,13 @@
+package com.ocp.day20;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class GroupingDemo2 {
+    public static void main(String[] args) {
+        List<String> sex = Arrays.asList("男", "男", "男", "女", "女", "女", "男", "男");
+        // 請問男女各有多少 ?
+        
+        
+    }
+}
