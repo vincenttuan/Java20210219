9d78649de25ecab718109481306660f21960b7bf
teacher <teacher@192.168.1.180>
2021-04-23 20:57:54 +0800

    Java OCP

--- src/main/java/com/ocp/day20/GroupingDemo2.java
+++ src/main/java/com/ocp/day20/GroupingDemo2.java
@@ -8,6 +8,8 @@ public class GroupingDemo2 {
         List<String> sex = Arrays.asList("男", "男", "男", "女", "女", "女", "男", "男");
         // 請問男女各有多少 ?
         
+        List<Integer> score = Arrays.asList(100, 30, 80, 40, 90);
+        // 請問及格與不及格各有多少 ?
         
     }
 }
