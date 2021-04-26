c465c12f44ae7c82c1893348c5b010ce1c4dc575
teacher <teacher@192.168.1.180>
2021-04-26 20:35:57 +0800

    java

---
+++ src/main/java/com/ocp/day21/CutGame.java
@@ -0,0 +1,12 @@
+package com.ocp.day21;
+
+/*
+請撰寫一支程式, 需由鍵盤輸入一個整數, 用來表示繩子的長度(單位:公尺)
+如果每天減去一半的長度要花費幾天的時間長度會短於5公尺
+繩子長度有1000與99這二條
+*/
+public class CutGame {
+    public static void main(String[] args) {
+        
+    }
+}
