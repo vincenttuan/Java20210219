e110bf881f89a722a3df88cf2c334e75bc742ec0
teacher <teacher@192.168.1.180>
2021-04-30 21:22:45 +0800

    java

---
+++ src/main/java/com/ocp/day23/Power.java
@@ -0,0 +1,15 @@
+package com.ocp.day23;
+
+// 電力換算
+// W(瓦數), V(電壓), A(電流)
+public class Power {
+    // 取得 W
+    public int getW(int v, int a) {
+        int w = v * a;
+        return w;
+    }
+    // 取得 V
+    
+    // 取得 A
+    
+}
