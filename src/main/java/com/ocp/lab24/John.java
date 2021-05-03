3888335faa8538be034c4e4a714d188bf57cd5c0
teacher <teacher@192.168.1.180>
2021-05-03 21:25:18 +0800

    java

---
+++ src/main/java/com/ocp/lab24/John.java
@@ -0,0 +1,7 @@
+package com.ocp.lab24;
+
+public class John extends Thread {
+    public void run() {
+        System.out.println("寫程式");
+    }
+}
---
+++ src/main/java/com/ocp/lab24/JohnMain.java
@@ -0,0 +1,8 @@
+package com.ocp.lab24;
+
+public class JohnMain {
+    public static void main(String[] args) {
+        John john = new John();
+        john.start();
+    }
+}
