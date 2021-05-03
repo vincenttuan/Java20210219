865d3f121f652a64023343742ee46a1af56421a3
teacher <teacher@192.168.1.180>
2021-05-03 21:32:48 +0800

    java

---
+++ src/main/java/com/ocp/lab24/Programming.java
@@ -0,0 +1,12 @@
+package com.ocp.lab24;
+
+// 要給執行緒執行的工作任務
+public class Programming implements Runnable {
+
+    @Override
+    public void run() { // 寫程式的工作
+        String tname= Thread.currentThread().getName();
+        System.out.println(tname + " 寫 Java 程式");
+    }
+    
+}
---
+++ src/main/java/com/ocp/lab24/Report.java
@@ -0,0 +1,12 @@
+package com.ocp.lab24;
+
+// 要給執行緒執行的工作任務
+public class Report implements Runnable {
+
+    @Override
+    public void run() { // 寫報表的工作
+        String tname= Thread.currentThread().getName();
+        System.out.println(tname + " 寫 PPT 報表");
+    }
+    
+}
