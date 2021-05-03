834eda1b945b90c36e33a1db7b43a81e7de66a09
teacher <teacher@192.168.1.180>
2021-05-03 21:51:24 +0800

    java

---
+++ src/main/java/com/ocp/lab24/Jobs.java
@@ -0,0 +1,17 @@
+package com.ocp.lab24;
+
+public class Jobs implements Runnable {
+    private Runnable[] runnables;
+
+    public Jobs(Runnable[] runnables) {
+        this.runnables = runnables;
+    }
+
+    @Override
+    public void run() {
+        for(Runnable r : runnables) {
+            //new Thread(r).start();
+            r.run();
+        }
+    }
+}
--- src/main/java/com/ocp/lab24/MaryMain.java
+++ src/main/java/com/ocp/lab24/MaryMain.java
@@ -6,5 +6,10 @@ public class MaryMain {
         Runnable job2 = new Report();
         Thread mary = new Thread(job1, "Mary");
         mary.start();
+        
+        Runnable[] runnables = {job1, job2};
+        Runnable job3 = new Jobs(runnables);
+        Thread helen = new Thread(job3, "Helen");
+        helen.start();
     }
 }
