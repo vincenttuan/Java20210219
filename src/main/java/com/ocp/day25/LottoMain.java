fb5ba5e418c991fee8135d698ed6a139a558c51b
teacher <teacher@192.168.1.180>
2021-05-07 20:47:52 +0800

    java

---
+++ src/main/java/com/ocp/day25/LottoMain.java
@@ -0,0 +1,23 @@
+package com.ocp.day25;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LottoMain {
+    public static void main(String[] args) {
+        FutureTask<Integer> task = new FutureTask<>(new Lotto());
+        new Thread(task).start();
+        System.out.println("計算中...");
+        
+        try {
+            System.out.println(task.get());
+        } catch (InterruptedException ex) {
+            
+        } catch (ExecutionException ex) {
+            
+        }
+        System.out.println("程式結束");
+    }
+}
