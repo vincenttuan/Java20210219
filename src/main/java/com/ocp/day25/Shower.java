d8c649960761a32c5e260170cd23a0b9681001f8
teacher <teacher@192.168.1.180>
2021-05-07 19:55:33 +0800

    java

--- src/main/java/com/ocp/day25/Shower.java
+++ src/main/java/com/ocp/day25/Shower.java
@@ -10,7 +10,7 @@ class Father extends Thread {
         worker.start();
         
         try {
-            worker.join();
+            worker.join(6000);
         } catch (Exception e) {
         }
         
@@ -22,7 +22,7 @@ class Father extends Thread {
 class Worker extends Thread {
     public void run() {
         System.out.println("瓦斯工人準備送瓦斯");
-        for(int i=1;i<=10;i++) {
+        for(int i=1;i<=5;i++) {
             try {
                 System.out.print(i + "秒鐘 ");
                 Thread.sleep(1000);
