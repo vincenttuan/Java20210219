47e8172bf042e2d0040d7c6cd2b993cc4d189729
teacher <teacher@192.168.1.180>
2021-05-10 21:10:49 +0800

    java

--- src/main/java/com/ocp/day27/Home.java
+++ src/main/java/com/ocp/day27/Home.java
@@ -2,6 +2,10 @@ package com.ocp.day27;
 
 public class Home {
     public static void main(String[] args) {
-        
+        Cookies cookies = new Cookies(10);
+        Thread master = new Thread(new Put(cookies));
+        Thread dog = new Thread(new Eat(cookies));
+        master.start();
+        dog.start();
     }
 }
