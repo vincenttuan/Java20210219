168c42a1a2fd2523666144dbfa5646f6a554b20e
teacher <teacher@192.168.1.180>
2021-05-10 21:00:40 +0800

    java

---
+++ src/main/java/com/ocp/day27/Cookies.java
@@ -0,0 +1,13 @@
+package com.ocp.day27;
+
+public class Cookies {
+    
+    public void put(int n) {
+        System.out.printf("主人放第 %d 塊餅乾\n", n);
+    }
+    
+    public void eat(int n) {
+        System.out.printf("小狗吃第 %d 塊餅乾\n", n);
+    }
+    
+}
