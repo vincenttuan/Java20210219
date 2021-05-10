a16371efa033b78356daa3f7b01c24926983f64d
teacher <teacher@192.168.1.180>
2021-05-10 21:07:56 +0800

    java

--- src/main/java/com/ocp/day27/Cookies.java
+++ src/main/java/com/ocp/day27/Cookies.java
@@ -11,11 +11,11 @@ public class Cookies {
         return amount;
     }
     
-    public void put(int n) {
+    public synchronized void put(int n) {
         System.out.printf("主人放第 %d 塊餅乾\n", n);
     }
     
-    public void eat(int n) {
+    public synchronized void eat(int n) {
         System.out.printf("小狗吃第 %d 塊餅乾\n", n);
     }
     
