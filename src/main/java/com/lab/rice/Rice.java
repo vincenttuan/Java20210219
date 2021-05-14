9ad913de86025f6be1c0b23cdf208e63898ff997
teacher <teacher@192.168.1.180>
2021-05-14 21:30:14 +0800

    java

--- src/main/java/com/lab/rice/Rice.java
+++ src/main/java/com/lab/rice/Rice.java
@@ -4,6 +4,7 @@ public class Rice {
     private String 品名;
     private String 不合格原因;
     private String 行政處分;
+    private String 檢驗結果;
 
     public String get品名() {
         return 品名;
@@ -29,9 +30,18 @@ public class Rice {
         this.行政處分 = 行政處分;
     }
 
+    public String get檢驗結果() {
+        return 檢驗結果;
+    }
+
+    public void set檢驗結果(String 檢驗結果) {
+        this.檢驗結果 = 檢驗結果;
+    }
+
     @Override
     public String toString() {
-        return "Rice{" + "\u54c1\u540d=" + 品名 + ", \u4e0d\u5408\u683c\u539f\u56e0=" + 不合格原因 + ", \u884c\u653f\u8655\u5206=" + 行政處分 + '}';
+        return "Rice{" + "\u54c1\u540d=" + 品名 + ", \u4e0d\u5408\u683c\u539f\u56e0=" + 不合格原因 + ", \u884c\u653f\u8655\u5206=" + 行政處分 + ", \u6aa2\u9a57\u7d50\u679c=" + 檢驗結果 + '}';
     }
+    
     
 }
