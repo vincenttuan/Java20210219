b0f9a4ef1f31b9101fb3188e61d6d1545419e070
teacher <teacher@192.168.1.180>
2021-05-14 21:27:40 +0800

    java

---
+++ src/main/java/com/lab/rice/Main.java
@@ -0,0 +1,12 @@
+package com.lab.rice;
+
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        Rice[] rices = Util.getRices();
+        Stream.of(rices)
+                .filter(r -> r.get品名().contains("冠軍"))
+                .forEach(System.out::println);
+    }
+}
