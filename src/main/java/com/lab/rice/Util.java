d6677c3c65d2f3f7842ef7952b9354426aedc210
teacher <teacher@192.168.1.180>
2021-05-14 21:33:35 +0800

    java

--- src/main/java/com/lab/rice/Util.java
+++ src/main/java/com/lab/rice/Util.java
@@ -5,7 +5,10 @@ import java.util.concurrent.FutureTask;
 
 public class Util {
     public static Rice[] getRices() {
-        String path = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        // 不合格
+        //String path = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        // 合格
+        String path = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceQualified.aspx";
         RiceJsonThread r1 = new RiceJsonThread(path);
         FutureTask<String> task = new FutureTask<>(r1);
         new Thread(task).start();
