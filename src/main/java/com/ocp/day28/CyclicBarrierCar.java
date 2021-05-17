77f6767fb81493fce3f09ecd97d589ff84b8bc3e
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-17 19:46:18 +0800

    Java

--- src/main/java/com/ocp/day28/CyclicBarrierCar.java
+++ src/main/java/com/ocp/day28/CyclicBarrierCar.java
@@ -6,8 +6,16 @@ import java.util.concurrent.CyclicBarrier;
 public class CyclicBarrierCar {
     public static void main(String[] args) {
         int n = 4;
-        CyclicBarrier cb = new CyclicBarrier(n);
-        
+        //CyclicBarrier cb = new CyclicBarrier(n);
+        Runnable r = () -> {
+            String tname = Thread.currentThread().getName();
+            System.out.printf("%s 說開動,吃飯!\n", tname);
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+            }
+        };
+        CyclicBarrier cb = new CyclicBarrier(n, r);
         new Car(cb).start();
         new Car(cb).start();
         new Car(cb).start();
