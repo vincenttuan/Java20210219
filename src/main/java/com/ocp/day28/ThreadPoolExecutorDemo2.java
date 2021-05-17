360813caa2dc20ee1e0358524a06be9360692e80
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-17 21:24:53 +0800

    Java

--- src/main/java/com/ocp/day28/ThreadPoolExecutorDemo2.java
+++ src/main/java/com/ocp/day28/ThreadPoolExecutorDemo2.java
@@ -1,5 +1,8 @@
 package com.ocp.day28;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 class Task implements Runnable {
@@ -22,7 +25,23 @@ class LongTask implements Runnable {
 }
 
 public class ThreadPoolExecutorDemo2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(new Task());
+        service.submit(new LongTask());
+        service.submit(new Task());
+        service.submit(new LongTask());
+        service.submit(new Task());
+        System.out.println("任務配置完畢");
         
+        // 平滑關閉
+        service.shutdown();
+        
+        // 偵測是否還有工作在執行
+        while (!service.awaitTermination(1, TimeUnit.SECONDS)) {            
+            System.out.println("還有工作在進行...");
+        }
+        
+        System.out.println("任務完成停止");
     }
 }
