184dbb07c92318330221d85487ad4f41047d471c
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-21 21:18:18 +0800

    Java

--- src/main/java/com/ocp/day30/ParallelExample2.java
+++ src/main/java/com/ocp/day30/ParallelExample2.java
@@ -1,16 +1,21 @@
 package com.ocp.day30;
 
 import java.util.stream.IntStream;
-
+/*
+.sequential() 預設 Normal (單工)
+.parallel() 平行 (多工)
+*/
 public class ParallelExample2 {
     public static void main(String[] args) {
         System.out.println("Normal");
         IntStream range = IntStream.rangeClosed(1, 10);
+        System.out.println(range.isParallel());
         range.forEach(System.out::println);
         
         System.out.println("Parallel"); // 平行
-        IntStream range2 = IntStream.rangeClosed(1, 10);
-        range2.parallel().forEach(System.out::println);
+        IntStream range2 = IntStream.rangeClosed(1, 10).parallel();
+        System.out.println(range2.isParallel());
+        range2.forEach(System.out::println);
         
     }
 }
