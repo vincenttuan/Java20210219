288e8a1f831f0f5999b575452e087a7f3134d676
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-24 20:43:58 +0800

    Java

--- src/main/java/com/ocp/day32_io/CreateFileDemo.java
+++ src/main/java/com/ocp/day32_io/CreateFileDemo.java
@@ -1,11 +1,27 @@
 package com.ocp.day32_io;
 
 import java.io.File;
+import java.io.IOException;
 
 public class CreateFileDemo {
     public static void main(String[] args) {
-        String path = "";
+        String path = "src/main/java/com/ocp/day32_io";
         String name = "data.txt";
-        File file = new File("");
+        String pathAddName = path + File.separator + name;
+        
+        File file = new File(pathAddName);
+        System.out.printf("%s 是否存在? %b\n", pathAddName, file.exists());
+        
+        // 若檔案不存在就建立新檔
+        if(!file.exists()) {
+            System.out.print("建立檔案: ");
+            try {
+                boolean success = file.createNewFile();
+                System.out.println(success);
+            } catch (IOException e) {
+                System.out.println(e);
+            }
+            
+        }
     }
 }
