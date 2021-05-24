7c5b5a53155aabc4abe7b0f7e8f5883e41cb5b99
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-24 19:32:44 +0800

    Java

---
+++ src/main/java/com/ocp/day31/LottoLock.java
@@ -0,0 +1,35 @@
+package com.ocp.day31;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
+public class LottoLock implements Runnable {
+    private String userNme;
+    private static final ReentrantLock LOCK = new ReentrantLock();
+    public LottoLock(String userNme) {
+        this.userNme = userNme;
+    }
+    
+    @Override
+    public void run() {
+        try {
+            LOCK.lock(); // 嘗試鎖定
+            IntStream.rangeClosed(1, 5).forEach(i -> {
+                try {
+                    Thread.sleep(3000);
+                } catch (Exception e) {
+                }
+                int number = new Random().nextInt(10);
+                System.out.printf("%s 得到第 %d 組號碼: %d, 時間 %s\n",
+                        userNme, i, number, new Date().toString());
+            });
+        } catch (Exception e) {
+            
+        } finally {
+            LOCK.unlock();
+        }
+    }
+    
+}
