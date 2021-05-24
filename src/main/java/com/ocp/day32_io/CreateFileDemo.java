3c8622560c8f0781ea02f63efd8a11cd2944cf62
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-24 20:35:54 +0800

    Java

---
+++ src/main/java/com/ocp/day32_io/CreateFileDemo.java
@@ -0,0 +1,11 @@
+package com.ocp.day32_io;
+
+import java.io.File;
+
+public class CreateFileDemo {
+    public static void main(String[] args) {
+        String path = "";
+        String name = "data.txt";
+        File file = new File("");
+    }
+}
