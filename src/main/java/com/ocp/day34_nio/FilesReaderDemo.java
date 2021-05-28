ff7d5c9ccd2c8d62b8ee2309e0d5f2436664ac8b
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-28 20:05:24 +0800

    Java

--- src/main/java/com/ocp/day34_nio/FilesReaderDemo.java
+++ src/main/java/com/ocp/day34_nio/FilesReaderDemo.java
@@ -10,16 +10,20 @@ public class FilesReaderDemo {
     public static void main(String[] args) throws Exception {
         Path path = Paths.get("src/main/java/com/ocp/day34_nio/1000 Sales Records.csv");
         
-        // Files.newBufferedReader
+        // 1. Files.newBufferedReader
         /*
         try(BufferedReader reader = Files.newBufferedReader(path)) {
             reader.lines().forEach(System.out::println);
         } catch (Exception e) {
         }
         */
-        // Files.readAllLines (List 集合)
-        List<String> list = Files.readAllLines(path);
-        list.stream().forEach(System.out::println);
+        
+        // 2. Files.readAllLines (List 集合)
+        //List<String> list = Files.readAllLines(path);
+        //list.stream().forEach(System.out::println);
+        
+        // 3. Files.lines (Stream 串流)
+        Files.lines(path).forEach(System.out::println);
         
     }
 }
