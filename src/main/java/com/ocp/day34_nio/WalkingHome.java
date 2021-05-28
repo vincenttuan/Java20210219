3d87d5c2e5aab6c4db85e2ca8dd2334040233c8c
vincenttuan <vincentjava@yahoo.com.tw>
2021-05-28 21:04:11 +0800

    Java

---
+++ src/main/java/com/ocp/day34_nio/WalkingHome.java
@@ -0,0 +1,28 @@
+package com.ocp.day34_nio;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
+
+public class WalkingHome {
+    public static void main(String[] args) throws IOException  {
+        Path homePath = Paths.get(System.getProperty("user.home"));
+        Stream<Path> files = Files.walk(homePath);
+        files.forEach(p -> {
+            Path path = p.toAbsolutePath();
+            System.out.print(p + " : ");
+            try {
+                System.out.println(Files
+                        .readAttributes(path, BasicFileAttributes.class)
+                        .creationTime());
+            } catch (IOException ex) {
+            }
+        });
+        
+    }
+}
